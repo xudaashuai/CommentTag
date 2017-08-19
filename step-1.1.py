ef0d8171a9b28390552aa950c87d3495b6f24970
徐大帅 <xudashuai0819@gmail.com>
2017-08-19 15:04:34 +0800

    第二步

--- step-1.1.py
+++ step-1.1.py
@@ -7,14 +7,18 @@ post = tdb.comments
 #client = redis.Redis(host='101.236.6.203', port=6379, db=0,password='19980819')
 
 nlp = Nlp('9688683', 'wndZFKVBmUTM5cfMb7C8UaOA', 'T80yWu0WAkbOFoKRVQ9p8lZMzj6rLq7S')
-for i in range(38,1000):
-    r = post.find().limit(100).skip(i * 100)
+for i in range(100):
+    r = post.find({'pro':None,'propadj':None}).limit(1000)
     t=[]
     for k, item in enumerate(r):
+        print(k)
         if not item['text']:
             continue
         text=item['text'].replace('#','')
         r=nlp.commentTag(text)
+        if 'error_code' in item:
+            if r['error_code'] !=282130:
+                print(r)
         propadj={}
         if 'items' in r:
             for it in r['items']:
